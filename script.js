0f668d9ccdb2b511b4678e622dedb88211781590
Caleb Cox <calebcox2010@gmail.com>
2022-01-01 22:40:06 -0600

    Updated title text with countdown functionality
    Added countdown javascript to title
    Updated title text html

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("testing");
+// Title Countdown Sequence
+let timeleft = 14;
+let startTime = document.getElementById("titleDigits");
+let countdownTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(countdownTimer);
+    startTime.style.fontStyle = "italic";
+    startTime.style.fontWeight = "900";
+    startTime.innerHTML = "15";
+
+  } else {
+    startTime.style.fontStyle = "italic";
+    startTime.style.fontWeight = "900";
+    startTime.innerHTML = timeleft.toString();
+  }
+  timeleft -= 1;
+}, 1000);
+
+// console.log("testing");
